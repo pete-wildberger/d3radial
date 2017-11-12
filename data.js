43654a7023598699b294f819923c07b0f4742265
Peter Wildberger <pete.wildberger@gmail.com>
2017-11-12 13:38:25 -0600

    profile part

---
+++ data.js
@@ -0,0 +1,83 @@
+var json = {
+  name: 'Pied-Piper',
+  reports: [
+    {
+      id: 49883,
+      user_id: 50621,
+      name: 'Richard Hendricks',
+      role: 'Admin',
+      avatar: '/assets/img_no_photo-tiny_thumb.png',
+      job_title: 'CEO',
+      department: 'Engineering',
+      reports: [
+        {
+          id: 49887,
+          user_id: 50630,
+          name: 'Bertram Gilfoyle',
+          role: 'Employee',
+          avatar: '/assets/img_no_photo-tiny_thumb.png',
+          job_title: 'Senior Systems Architect',
+          department: 'Engineering'
+        },
+        {
+          id: 49888,
+          user_id: 50631,
+          name: 'Dinesh Chugtai',
+          role: 'Employee',
+          avatar: '/assets/img_no_photo-tiny_thumb.png',
+          job_title: 'Senior Programmer',
+          department: 'Engineering'
+        },
+        {
+          id: 49889,
+          user_id: 50632,
+          name: 'Jared Dunn',
+          role: 'Admin',
+          avatar: '/assets/img_no_photo-tiny_thumb.png',
+          job_title: 'Head of Business Development',
+          department: 'Human Resources',
+          reports: [
+            {
+              id: 49893,
+              user_id: 50636,
+              name: 'Carla Walton',
+              role: 'Employee',
+              avatar: '/assets/img_no_photo-tiny_thumb.png',
+              job_title: 'Platform Engineer',
+              department: 'Engineering'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 49894,
+      user_id: 50628,
+      name: 'Erlich Bachman',
+      role: 'Manager',
+      avatar: '/assets/img_no_photo-tiny_thumb.png',
+      job_title: 'Major Investor',
+      department: 'Investor',
+      reports: [
+        {
+          id: 49891,
+          user_id: 50633,
+          name: 'Jian Yang',
+          role: 'Employee',
+          avatar: '/assets/img_no_photo-tiny_thumb.png',
+          job_title: 'Developer',
+          department: 'Engineering'
+        }
+      ]
+    },
+    {
+      id: 49885,
+      user_id: 50629,
+      name: 'Nelson Bighetti',
+      role: 'Employee',
+      avatar: '/assets/img_no_photo-tiny_thumb.png',
+      job_title: 'Major Investor',
+      department: 'Investor'
+    }
+  ]
+};
