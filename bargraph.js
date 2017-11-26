5488536bbfa6c39bc68df3e305aacf8a8d6d78de
Peter Wildberger <pete.wildberger@gmail.com>
2017-11-26 16:13:05 -0600

    x axis working

--- bargraph.js
+++ bargraph.js
@@ -1,33 +1,42 @@
 let bar = [100, 250, 175, 200, 120];
 let rectWidth = 100;
-let height = 300;
+let barHeight = 300;
+
+var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
 // getting min and max of data for scale
 let extent = d3.extent(bar);
 console.log('extent ', extent);
-
+let xExtent = d3.extent(bar, (d, i) => i + 1);
+console.log('xExtent', xExtent);
 // building scale for graph
 let yScale = d3
   .scaleLinear()
   .domain(extent)
-  .range([height, 0]);
+  .range([barHeight, 0]);
 console.log('yScale ', yScale);
 
 let xScale = d3
-  .scaleLinear()
-  .domain(bar.length)
-  .range([bar.length, 0]);
+  .scaleBand()
+  .domain(xExtent)
+  .range([margin.left, (bar.length + 1) * rectWidth - margin.right]);
 console.log('xScale ', xScale);
 
 // main bargraph
-let graph = d3.select('.bargraph').append('svg');
+let graph = d3
+  .select('.bargraph')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom);
 graph
   .selectAll('rect')
   .data(bar)
   .enter()
   .append('rect')
   .attr('x', (d, i) => i * rectWidth)
-  .attr('y', d => height - d)
+  .attr('y', d => barHeight - d)
   .attr('width', rectWidth)
   .attr('height', d => d)
   .attr('fill', d => {
@@ -53,9 +62,8 @@ let xAxis = d3.axisBottom().scale(xScale);
 d3
   .select('svg')
   .append('g')
-  .attr('transform', 'translate(0, 30)')
+  .attr('transform', 'translate(0,' + barHeight + ')')
   .call(xAxis);
-console.log('xAxis ', xAxis);
 
 // yAxis tick styling
 // axis.tickFormat()
